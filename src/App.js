b62337e5dae77abb19964ceabcb56a1848e4044f
Arturo Mendoza <dizsel74@gmail.com>
2023-06-13 14:43:07 -0600

    Anexo de estilos y formato de testimoios

--- src/App.js
+++ src/App.js
@@ -6,7 +6,36 @@ import Testimonio from './componetes/Testimonio.jsx';
 function App() {
   return (
     <div className="App">
-      <Testimonio />
+      <div className="contenedor-principal">
+        <h1>Here is what our alumni say about freeCodeCamp:</h1>
+        <Testimonio 
+          name={'Emma'} 
+          apellido={'Bostian'}
+          ciudad={'Suecia'} 
+          carrera={'Ingeniero en Sistemas'} 
+          empresa={'Spotify'}
+          imagen={'Emma'}
+          testimonio={"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."}
+        />
+        <Testimonio 
+          name={'Shawn'}
+          apellido={'Wang'}
+          ciudad={'Singapur'} 
+          carrera={'Web Developer'} 
+          empresa={'Amazon'}
+          imagen={'Shawn'}
+          testimonio={"It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."}
+          />
+        <Testimonio 
+          name={'Sarah'} 
+          apellido={'Chima'}
+          ciudad={'Nigeria'} 
+          carrera={'Ingeniero en Software'} 
+          empresa={'ChatDesk'}
+          imagen={'Sarah'}
+          testimonio={"freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."}
+          />
+      </div>
     </div>
   );
 }
--- src/componetes/Testimonio.jsx
+++ src/componetes/Testimonio.jsx
@@ -1,19 +1,28 @@
 import React from "react";
+import  "../hojas-de-estilo/Testimonio.css"
 
-function Testimonio(){
+function Testimonio(props){
   return(
     <div className='contenedor-testimonio'>
       <img 
       className='imagen-testimonio' 
-      src={require('../imagenes/testimonio-Emma.png')}
-      alt='Foto de usuario'/>
+      src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+      alt={`Foto de ${props.name} ${props.apellido}`} />
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
-        <p className='cargo-testimonio'>Ingeniero en Software en Spotify</p>
-        <p className='texto-testimonio'>"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure sequi provident ullam quibusdam accusantium! Corporis, eos commodi. Sunt, nemo dolores cum, delectus quo rerum accusantium veniam inventore itaque, officiis aliquid."</p>
+        <p className='nombre-testimonio'><b>{props.name} {props.apellido}</b> en {props.ciudad}</p>
+        <p className='cargo-testimonio'> {props.carrera} en <b>{props.empresa}</b></p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
       </div>
     </div>
   );
 }
-
+ Testimonio.defaultProps = {
+  name:'Nombre',
+  apellido:'Apellido',
+  ciudad: 'Ciudad',
+  carrera: 'Profesion',
+  empresa: 'Empresa',
+  imagen: 'Sarah',
+  testimonio: 'lorem ipsum lorem ipsum lorem ipsum',
+}
 export default Testimonio;
